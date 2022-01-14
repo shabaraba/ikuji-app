29ed057411e8670d995702075b128ee696cb79ee
shabaraba <work.shabaraba@gmail.com>
2022-01-14 13:51:45 +0900

    - show memory list using dummy data.

--- src/app/screens/TabThreeScreen.tsx
+++ src/app/screens/TabThreeScreen.tsx
@@ -1,34 +1,72 @@
-import React, {useState} from 'react';
-import { StyleSheet } from 'react-native';
+import React, {useState, useEffect, useMemo} from 'react';
+import { StyleSheet, FlatList, ListRenderItem, ListRenderItemInfo } from 'react-native';
 import {Calendar, CalendarList, Agenda, CalendarProps} from 'react-native-calendars';
+
+import { format } from 'date-fns';
+
+import DummyData from '../test/dummy_data.json';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function TabThreeScreen() {
-    const [selected, setSelected] = useState('2022-01-01');
+interface MemoryHead {
+    id: number
+    title: string
+    date: Date
+}
 
-    const vacation = {key: 'vacation', color: 'red', selectedDotColor: 'blue'};
-    const massage = {key: 'massage', color: 'blue', selectedDotColor: 'blue'};
-    const workout = {key: 'workout', color: 'green'};
+export default function TabThreeScreen() {
+    const [selected, setSelected] = useState<Date>(new Date('2022-01-01'));
+    const [memoryList, setMemoryList] = useState<MemoryHead[]>();
+
+    useEffect(() => {
+        // console.log(`tap on ${format(selected, 'yyyy-MM-dd')}`);
+        const filteredArray = DummyData.filter(
+            data => data.date == format(selected, 'yyyy-MM-dd')
+        );
+        setMemoryList(filteredArray.map(data => {
+            return {
+                id: data.id,
+                title: data.title,
+                date: new Date(data.date)
+            }
+        }));
+    }, [selected]);
 
     const onDayPress: CalendarProps['onDayPress'] = day => {
-        setSelected(day.dateString);
+        setSelected(new Date(day.dateString));
       };
+
+    const renderItem: ListRenderItem<MemoryHead> = ({ item }) => (
+        <View style={styles.container}>
+            <View>
+                <Text>{item.title}</Text>
+            </View>
+        </View>
+    );
+
     return(
-        <Calendar
-            monthFormat={'yyyy年 MM月'}
-            markingType={'multi-dot'}
-            onDayPress={onDayPress}
-            markedDates={{
-                [selected]: {
-                  selected: true,
-                  disableTouchEvent: true,
-                  selectedColor: 'orange',
-                  selectedTextColor: 'red'
-                }
-            }}
-        />
+        <View>
+            <Calendar
+                monthFormat={'yyyy年 MM月'}
+                markingType={'multi-dot'}
+                onDayPress={onDayPress}
+                markedDates={{
+                    [format(selected, 'yyyy-MM-dd')]: {
+                      selected: true,
+                      disableTouchEvent: true,
+                      selectedColor: 'orange',
+                      selectedTextColor: 'red'
+                    }
+                }}
+            />
+            <FlatList 
+                data={memoryList}
+                renderItem={renderItem}
+                keyExtractor={memoryHead => `${memoryHead.id}`}
+                extraData={memoryList}
+            />
+        </View>
     )
 }
 
